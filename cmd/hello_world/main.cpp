2e432bf2b949cd782d149417d569209cd2939b5e
Ilya Volkov <ilya@lispberry.dev>
2023-11-21 21:18:04 +0500

    WIP

--- cmd/hello_world/main.cpp
+++ cmd/hello_world/main.cpp
@@ -1,10 +1,10 @@
 #include <cstdio>
 
-int main(int argc, char **argv) {
+   int main(int argc, char **argv) {
   if (argc != 2) {
     return 1;
   }
-  printf(argv[1]);
+  printf("%s\n", argv[1]);
 
   return 0;
 }
